29b3019b6313765ca89c858cbd0993924f94d19c
Daniel Foote <freefoote@dview.net>
2013-01-08 08:57:18 +0800

    Added a setup script to the project.

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+	name='pminterface',
+	description="Paasmaker Python interface library",
+	author="Daniel Foote",
+	author_email="freefoote@paasmaker.com",
+	url="http://paasmaker.com",
+	version='0.9',
+	packages=['pminterface']
+)
